77b9facac3b180dc85a1f0d330ffb690007b7d81
kaiqueav <kaiquecerq66@outlook.com>
2024-11-08 01:16:57 -0300

    feat: create post route

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { ProductDto } from './product.dto';
+import { product } from '@prisma/client';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+    constructor(private readonly productService:ProductService ){}
+    @Post()
+    async create(@Body() productDto:ProductDto):Pormise<product>{
+        return this.productService.create(productDto)
+    }
+
+
+}
