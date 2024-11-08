3c38f20de0544b8f863be2edeaee71e42f50357d
kaiqueav <kaiquecerq66@outlook.com>
2024-11-07 23:29:29 -0300

    config  bd aand instaler payment

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  
+  constructor() {
+    super();
+  }
+
+  // Conecta ao banco de dados quando o módulo é inicializado
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  // Desconecta do banco de dados quando o módulo é destruído
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  // Método auxiliar para limpar dados do banco de dados (para testes, por exemplo)
+  async cleanDatabase() {
+    await this.$transaction([
+      this.order.deleteMany(),
+      this.client.deleteMany(),
+      this.product.deleteMany(),
+    ]);
+  }
+}
