ad620b839b4730192c4e499148dc7a64cb3eaf8e
kaiqueav <kaiquecerq66@outlook.com>
2024-11-08 00:42:09 -0300

    feat:  create product service, generate  product

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
-
+import { PrismaService } from 'src/prisma/prisma.module';
+import { ProductDto } from './product.dto';
 @Injectable()
-export class ProductService {}
+export class ProductService {
+    constructor ( private prisma: PrismaService){}
+
+    async create(productDto: ProductDto){
+        return  this.prisma.product.create({
+            data: productDto,
+        });
+    }
+
+
+}
