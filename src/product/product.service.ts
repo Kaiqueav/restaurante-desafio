d68025406d37df3bcd33aac7d1c21b2b6dfffc3b
kaiqueav <kaiquecerq66@outlook.com>
2024-11-08 01:55:02 -0300

    feat: exceptions

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.module';
 import { ProductDto } from './product.dto';
 import { product } from '@prisma/client';
@@ -7,9 +7,28 @@ export class ProductService {
     constructor ( private prisma: PrismaService){}
 
     async create(productDto: ProductDto){
-        return  this.prisma.product.create({
-            data: productDto,
-        });
+        if (!productDto.name || !productDto.description || !productDto.price || !productDto.type) {
+            throw new HttpException('All fields are required', HttpStatus.BAD_REQUEST);
+          }
+      
+          // Verifica se o preço é um número válido
+          if (isNaN(productDto.price) || productDto.price <= 0) {
+            throw new HttpException('Price must be a positive number', HttpStatus.BAD_REQUEST);
+          }
+         try{ 
+            return await this.prisma.product.create({
+            data:{
+                ...productDto,
+                price: Number(productDto.price)
+                },
+            });
+          }catch(error){
+            throw new HttpException(
+                'Failed to create product: ' + error.message,
+                HttpStatus.INTERNAL_SERVER_ERROR,
+              );
+          }
+          
     }
 
     async findAll(): Promise<product[]>{
