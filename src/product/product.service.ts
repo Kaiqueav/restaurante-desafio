ea1b75bfbb961fbef6d41c81120f986dfaec8b48
kaiqueav <kaiquecerq66@outlook.com>
2024-11-08 01:02:20 -0300

    feat: create   methods,  for get, put, deleteand  update

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.module';
 import { ProductDto } from './product.dto';
+import { product } from '@prisma/client';
 @Injectable()
 export class ProductService {
     constructor ( private prisma: PrismaService){}
@@ -10,6 +11,31 @@ export class ProductService {
             data: productDto,
         });
     }
+
+    async findAll(): Promise<product[]>{
+        return this.prisma.product.findMany();
+    }
+    async findBy(id:number): Promise<product>{
+        return this.prisma.product.findUnique({
+            where:{id},
+        });
+    }
+    async update(id: number, productDto:ProductDto): Promise<product>{
+        return this.prisma.product.update({
+            where:{id},
+            data:{
+                name: productDto.name|| undefined,
+                description: productDto.description|| undefined,
+                price: productDto.price|| undefined,
+                type: productDto.type||undefined,
+            },
+        });
+    }
+    async remove(id: number): Promise<product> {
+        return this.prisma.product.delete({
+          where: { id },
+        });
+      }
 
 
 }
