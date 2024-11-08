cc54a664f9de5cabd2e618ca1b50abea2f9d60ec
kaiqueav <kaiquecerq66@outlook.com>
2024-11-08 01:24:03 -0300

    feat: create findAll and findBy routes

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductDto } from './product.dto';
 import { product } from '@prisma/client';
@@ -7,9 +7,16 @@ import { product } from '@prisma/client';
 export class ProductController {
     constructor(private readonly productService:ProductService ){}
     @Post()
-    async create(@Body() productDto:ProductDto):Pormise<product>{
+    async create(@Body() productDto:ProductDto):Promise<product>{
         return this.productService.create(productDto)
     }
-
+    @Get()
+    async  findAll():Promise<product[]>{
+        return this.productService.findAll();
+    }
+    @Get(':id')
+    async finBy(@Param('id')id:number):Promise<product>{
+        return this.productService.findBy(id);
+    }
 
 }
